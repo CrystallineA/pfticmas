0ed79a49359cbd849de3d6f2886ccb673cfd1476
Catalina Alcalde <catalinaalcalde92@gmail.com>
2023-01-30 18:22:23 -0300

    comit inicial

---
+++ interacciones.js
@@ -0,0 +1,11 @@
+
+
+var nombre = "Radoslav";
+var apellido = "Hadžić";
+
+var concatenacion = nombre + " " + apellido;
+
+var nombre_completo = document.getElementById("nombre_completo");
+nombre_completo.innerHTML = concatenacion;
+
+
